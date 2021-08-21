c28de591ac3eb56b4bd72159f20a1dfae5c6ebc4
jungyuuu <83994393+jungyuuu@users.noreply.github.com>
2021-08-21 16:44:17 +0900

    Update soc.py

--- src/vision/soc.py
+++ src/vision/soc.py
@@ -11,12 +11,20 @@ def send(send_data):
     client_socket.sendall(send_data.encode())
     return send_data
 
+
+def connect():
+    try:
+        client_socket.connect((ip, port))
+        print("connected")
+    except socket.error as e:
+        print(f"error while connecting : {e}")
+        return False
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 서버
 ip = '127.0.0.1'
 port = 9999
-client_socket.connect((ip, port))
 
 recv_thread = threading.Thread(target=recv, args=(client_socket,))
 send_thread = threading.Thread(target=send, args=(client_socket,))
