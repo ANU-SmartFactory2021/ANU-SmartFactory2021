48255547524065ffed8d7c2892bf990434929c1d
movejin <t_o159@naver.com>
2021-09-04 13:03:39 +0900

    업데이트

--- src/machine/main_method.py
+++ src/machine/main_method.py
@@ -2,31 +2,32 @@
 #import trctmethod as tf #trctmethod_function
 import RPi.GPIO as GPIO
 import time
-
+import servomethod as sf
+import trctmethod as tf
 
 
 
 def main_method():
     while True:
-        print(" 서보 함수 호출: 1 \n 센서함수 호출: 2")
+        #print(" 서보 함수 호출: 1 \n 센서함수 호출: 2")
         bt = 0
         bt = input()
             
         if bt == '1':
-            import servomethod as sf
-            print("서보 함수 호출")
-            sf.servo_main()
-                #break
-                
+            sf.servo_main(bt)
+            
         elif bt == '2':
-            import trctmethod as tf
-            print("센서 함수 호출")
-            tf.trct_main()
-                #break
+            sf.servo_main(bt)
+                
+        elif bt == '3':
+            sensor_state = tf.read_sensor()
+            print( "센서 상태 : ", sensor_state )
+        
+        elif bt == 'x':
+            break
         
         else :
             print("다시 입력해주세요")
-            break
         
 try:
     main_method()
