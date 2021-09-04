0213f4c18387e5b171dca4de31c678d8e9ef11cb
jungyuuu <83994393+jungyuuu@users.noreply.github.com>
2021-09-04 09:47:18 +0900

    비전 업데이트

--- src/vision/qr.py
+++ src/vision/qr.py
@@ -2,8 +2,11 @@
 import cv2
 import base64
 import os
-
+import numpy as np
 from pyzbar import pyzbar
+
+cap = cv2.VideoCapture(0)
+
 
 def check():
     try:
@@ -15,21 +18,23 @@ def check():
     except cv2.error as e:
         print(e)
 
-def decode():
-    #카메라 주소
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
 
+def decode():
+    # 카메라 주소
+    retval, frame = cap.read()
+
+    cv2.imwrite('image.jpg', frame)
     decoded_objects = pyzbar.decode(frame)
-    cv2.imwrite("image.jpg", frame)
-    with open("image.jpg", "rb") as img_file:
+
+    with open('image.jpg', 'rb') as img_file:
         converted_string = base64.b64encode(img_file.read())
 
-    os.remove("image.jpg")
-    print(decoded_objects)
+    os.remove('image.jpg')
 
     if not decoded_objects:
+        print(decoded_objects)
         return "QR_READ_ERROR"
     else:
         for obj in decoded_objects:
+            print(obj.data)
             return f'<QR:{obj.data}|{converted_string.decode("utf-8")}>'
