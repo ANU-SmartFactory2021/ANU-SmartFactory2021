cc9e4a1a4059f4cdf4600064c0b9330f5c6c4dc2
jungyuuu <83994393+jungyuuu@users.noreply.github.com>
2021-09-11 14:23:54 +0900

    update qr

--- src/vision/qr.py
+++ src/vision/qr.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 import cv2
 import base64
-import os
-import numpy as np
+import numpy
 from pyzbar import pyzbar
 
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
 
 def check():
     try:
-        cap = cv2.VideoCapture(0)
         if not cap.isOpened():
+            print('capture fail')
+            cap.release()
             return False
         else:
+            print('capture ok')
             return True
     except cv2.error as e:
         print(e)
@@ -22,8 +25,8 @@ def check():
 
 def decode():
     retval, frame = cap.read()
-    retval, buffer = cv2.imencode('.jpg', frame)
     decoded_objects = pyzbar.decode(frame)
+    retval, buffer = cv2.imencode('.jpg', frame)
     converted_string = base64.b64encode(buffer)
 
     if not decoded_objects:
@@ -33,3 +36,6 @@ def decode():
         for obj in decoded_objects:
             print(obj.data)
             return f'<QR:{obj.data}|{converted_string.decode("utf-8")}>'
+
+# raspistill -v -o test.jpg
+# vcgencmd get_camera
--- src/vision/soc.py
+++ src/vision/soc.py
@@ -1,8 +1,10 @@
+
 # -*- coding: utf-8 -*-
 
 import socket
 import threading
-
+import errno
+import select
 
 def recv():
     try:
@@ -13,13 +15,19 @@ def recv():
 
 
 def send(send_data):
-    client_socket.sendall(send_data.encode())
-    return send_data
+    try:
+        client_socket.sendall(send_data.encode())
+    except socket.error as e:
+        if e.errno != errno.EAGAIN:
+            raise e
+        print('Blocking with', len(send_data), 'remaining')
+        select.select([], [client_socket], [])
 
 
 def connect():
     try:
         client_socket.connect((ip, port))
+        client_socket.setblocking(False)
         print("connected")
     except socket.error as e:
         print(f"error while connecting : {e}")
@@ -27,10 +35,10 @@ def connect():
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.setblocking(False)
+#client_socket.setblocking(False)
 
 # 서버
-ip = '127.0.0.1'
+ip = '192.168.0.2'
 port = 9999
 
 # recv_thread = threading.Thread(target=recv, args=(client_socket,))
