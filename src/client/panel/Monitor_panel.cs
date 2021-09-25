2343d7327e83a83c2641560c1e3eb845fe8e5f96
KwakYeonHo <dkfks3988@gmail.com>
2021-09-25 17:02:20 +0900

    0925 작업
    0925 작업

--- src/client/panel/Monitor_panel.cs
+++ src/client/panel/Monitor_panel.cs
@@ -19,15 +19,21 @@ namespace Client.panel
         OracleConnection conn;
         OracleCommand cmd;
 
+        public delegate void DataPassEventHandler(object sender);
+        public event DataPassEventHandler DataPassEvent;
+        public int state;
+
         string qual = " ";
         int product_num = 0;
         int qr_num = 0;
-
+        
+        int st_num = 0;
+        
         public Monitor_panel()
         {
             InitializeComponent();
             conn = new OracleConnection(strConn);
-            conn.Open();
+            conn.Open();           
             cmd = new OracleCommand();
             cmd.Connection = conn;
             MainForm.sc.set_recv_callback(recv_callback);            
@@ -36,9 +42,11 @@ namespace Client.panel
         public void FieldClear()
         {
             M_Company_txt.Text = " ";
-            M_Kcal_txt.Text = " ";
+            output_num_txt.Text = " ";
             M_Product_txt.Text = " ";
             M_Serialnum_txt.Text = " ";
+
+            st_num = 0;
         }
                 
         private void M_Serialnum_txt_TextChanged(object sender, EventArgs e)
@@ -48,15 +56,14 @@ namespace Client.panel
                 
                 string m_serialnum = M_Serialnum_txt.Text;
 
-                cmd.CommandText = $"select PRODUCT_NAME, KCAL, COMPANY from product where PRODUCT_ID = '{m_serialnum}'";
+                cmd.CommandText = $"select PRODUCT_NAME, COMPANY from product where PRODUCT_ID = '{m_serialnum}'";
                 OracleDataReader rdr = cmd.ExecuteReader();
 
                 while(rdr.Read())
                 {
                     string product_name = rdr["PRODUCT_NAME"].ToString();
                     M_Product_txt.Text = product_name;
-                    string kcal = rdr["KCAL"].ToString();
-                    M_Kcal_txt.Text = kcal;
+                    
                     string company = rdr["COMPANY"].ToString();
                     M_Company_txt.Text = company;
                 }
@@ -64,7 +71,7 @@ namespace Client.panel
         }
 
         delegate void del_draw_view(string _msg);
-        private void draw_view( string _msg )
+        public void draw_view( string _msg )
         {
 
             //MessageBox.Show(_msg);
@@ -72,13 +79,27 @@ namespace Client.panel
             Console.WriteLine(_msg);
             string[] result = _msg.Split(new char[] { '|' });
 
-            if (result.Length > 2)
+            if (result.Length > 1)
             {
 
                 byte[] imageBytes = Convert.FromBase64String(result[2]);
                 System.Drawing.ImageConverter converter = new System.Drawing.ImageConverter();
                 Image img = (Image)converter.ConvertFrom(imageBytes);
                 pictureBox1.Image = img;
+                Console.WriteLine(result[1]);
+                
+                if (result[1] == "QUALITY=PASS")
+                {
+                    good_txt.BackColor = Color.Green;
+                    Delay(1000);
+                    good_txt.BackColor = Color.White;
+                }
+                else if(result[1] == "QUALITY=FAIL")
+                {
+                    bad_txt.BackColor = Color.Red;
+                    Thread.Sleep(1000);
+                    bad_txt.BackColor = Color.White;
+                }
 
                 qual = result[1].Substring(8);
                 Console.WriteLine(qual);
@@ -87,21 +108,51 @@ namespace Client.panel
                 qr_num = Convert.ToInt32(result[0].Substring(7));
                 Console.WriteLine(qr_num);
 
-                if (result[1] == "QUALITY=PASS")
-                {
-                    good_txt.BackColor = Color.Green;
-                    Thread.Sleep(1000);
-                    good_txt.BackColor = Color.White;
-                }
-
                 save();
             }
             else
             {
+                //카메라 켜짐
                 if (_msg == "CMD=PI_ONE_READY")
                 {
                     cam_off.BackColor = Color.White;
                     cam_on.BackColor = Color.Green;
+                }
+                //적외선 센서 켜짐
+                else if(_msg == "CMD=IS")
+                {
+                    sensor_off.BackColor = Color.White;
+                    sensor_on.BackColor = Color.Green;
+                }
+                //컨베이어 벨트 작동
+                else if(_msg == "CMD=RUN")
+                {
+                    belt_off.BackColor = Color.White;
+                    belt_on.BackColor = Color.Green;
+                }
+                //적외선 센서 꺼짐
+                else if(_msg == "CMD=REMOVE")
+                {
+                    sensor_on.BackColor = Color.White;
+                    sensor_off.BackColor = Color.Red;
+                }
+
+                else if (_msg == "CMD=LAST")
+                {
+                    st_num++;
+                    output_num_txt.Text = st_num.ToString();                                      
+                }
+
+                else if(_msg == "CMD=FINISH")
+                {
+                    state = 3;
+                    DataPassEvent(state);
+                }
+                
+                else if (_msg == "CMD=EMERGENCY")
+                {
+                    state = 11;
+                    DataPassEvent(state);
                 }
             }
         }
@@ -124,5 +175,21 @@ namespace Client.panel
             cmd.ExecuteNonQuery();                      
         }
 
+        //딜레이 함수
+        private static DateTime Delay(int MS)
+
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+
+            return DateTime.Now;
+        }        
     }
 }
--- src/client/panel/Start_panel.Designer.cs
+++ src/client/panel/Start_panel.Designer.cs
@@ -29,170 +29,176 @@ namespace Client.panel
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.Company_txt = new System.Windows.Forms.TextBox();
-            this.Outputnum_txt = new System.Windows.Forms.TextBox();
+            this.Kcal_txt = new System.Windows.Forms.TextBox();
             this.Serialnum_txt = new System.Windows.Forms.TextBox();
             this.Product_combo = new System.Windows.Forms.ComboBox();
             this.Start_btn = new System.Windows.Forms.Button();
             this.Cancel_btn = new System.Windows.Forms.Button();
             this.product_png = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.product_png)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Start_panel";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 94);
+            this.label2.Location = new System.Drawing.Point(66, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.Size = new System.Drawing.Size(49, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "제 품 명";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 144);
+            this.label3.Location = new System.Drawing.Point(16, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.Size = new System.Drawing.Size(53, 12);
             this.label3.TabIndex = 2;
             this.label3.Text = "제조회사";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 189);
+            this.label4.Location = new System.Drawing.Point(16, 106);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.Size = new System.Drawing.Size(53, 12);
             this.label4.TabIndex = 3;
             this.label4.Text = "일련번호";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(42, 236);
+            this.label5.Location = new System.Drawing.Point(16, 144);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.Size = new System.Drawing.Size(49, 12);
             this.label5.TabIndex = 4;
-            this.label5.Text = "수      량";
+            this.label5.Text = "칼 로 리";
             // 
             // Company_txt
             // 
-            this.Company_txt.Location = new System.Drawing.Point(128, 140);
-            this.Company_txt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Company_txt.Location = new System.Drawing.Point(91, 70);
             this.Company_txt.Name = "Company_txt";
-            this.Company_txt.Size = new System.Drawing.Size(138, 25);
+            this.Company_txt.Size = new System.Drawing.Size(121, 21);
             this.Company_txt.TabIndex = 5;
             // 
-            // Outputnum_txt
+            // Kcal_txt
             // 
-            this.Outputnum_txt.Location = new System.Drawing.Point(128, 232);
-            this.Outputnum_txt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Outputnum_txt.Name = "Outputnum_txt";
-            this.Outputnum_txt.Size = new System.Drawing.Size(138, 25);
-            this.Outputnum_txt.TabIndex = 6;
+            this.Kcal_txt.Location = new System.Drawing.Point(91, 144);
+            this.Kcal_txt.Name = "Kcal_txt";
+            this.Kcal_txt.Size = new System.Drawing.Size(121, 21);
+            this.Kcal_txt.TabIndex = 6;
             // 
             // Serialnum_txt
             // 
-            this.Serialnum_txt.Location = new System.Drawing.Point(128, 185);
-            this.Serialnum_txt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Serialnum_txt.Location = new System.Drawing.Point(91, 106);
             this.Serialnum_txt.Name = "Serialnum_txt";
-            this.Serialnum_txt.Size = new System.Drawing.Size(138, 25);
+            this.Serialnum_txt.Size = new System.Drawing.Size(121, 21);
             this.Serialnum_txt.TabIndex = 7;
             // 
             // Product_combo
             // 
             this.Product_combo.FormattingEnabled = true;
-            this.Product_combo.Location = new System.Drawing.Point(128, 90);
-            this.Product_combo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Product_combo.Location = new System.Drawing.Point(141, 57);
             this.Product_combo.Name = "Product_combo";
-            this.Product_combo.Size = new System.Drawing.Size(138, 23);
+            this.Product_combo.Size = new System.Drawing.Size(121, 20);
             this.Product_combo.TabIndex = 8;
             this.Product_combo.SelectedIndexChanged += new System.EventHandler(this.Product_combo_SelectedIndexChanged);
             // 
             // Start_btn
             // 
-            this.Start_btn.Location = new System.Drawing.Point(499, 304);
-            this.Start_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Start_btn.Location = new System.Drawing.Point(317, 293);
             this.Start_btn.Name = "Start_btn";
-            this.Start_btn.Size = new System.Drawing.Size(130, 55);
+            this.Start_btn.Size = new System.Drawing.Size(114, 44);
             this.Start_btn.TabIndex = 9;
-            this.Start_btn.Text = "작업 시작";
+            this.Start_btn.Text = "시   작";
             this.Start_btn.UseVisualStyleBackColor = true;
             this.Start_btn.Click += new System.EventHandler(this.Start_btn_Click);
             // 
             // Cancel_btn
             // 
-            this.Cancel_btn.Location = new System.Drawing.Point(499, 366);
-            this.Cancel_btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Cancel_btn.Location = new System.Drawing.Point(437, 293);
             this.Cancel_btn.Name = "Cancel_btn";
-            this.Cancel_btn.Size = new System.Drawing.Size(130, 55);
+            this.Cancel_btn.Size = new System.Drawing.Size(114, 44);
             this.Cancel_btn.TabIndex = 10;
-            this.Cancel_btn.Text = "취    소";
+            this.Cancel_btn.Text = "완   료";
             this.Cancel_btn.UseVisualStyleBackColor = true;
             this.Cancel_btn.Click += new System.EventHandler(this.Cancel_btn_Click);
             // 
             // product_png
             // 
-            this.product_png.Location = new System.Drawing.Point(314, 90);
-            this.product_png.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.product_png.Location = new System.Drawing.Point(332, 27);
             this.product_png.Name = "product_png";
-            this.product_png.Size = new System.Drawing.Size(155, 169);
+            this.product_png.Size = new System.Drawing.Size(216, 188);
             this.product_png.TabIndex = 11;
             this.product_png.TabStop = false;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.Company_txt);
+            this.groupBox2.Controls.Add(this.Kcal_txt);
+            this.groupBox2.Controls.Add(this.Serialnum_txt);
+            this.groupBox2.Location = new System.Drawing.Point(50, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 188);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "제품 정보";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(406, 227);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "제품 이미지";
+            // 
             // Start_panel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.product_png);
             this.Controls.Add(this.Cancel_btn);
             this.Controls.Add(this.Start_btn);
             this.Controls.Add(this.Product_combo);
-            this.Controls.Add(this.Serialnum_txt);
-            this.Controls.Add(this.Outputnum_txt);
-            this.Controls.Add(this.Company_txt);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Controls.Add(this.groupBox2);
             this.Name = "Start_panel";
-            this.Size = new System.Drawing.Size(673, 440);
+            this.Size = new System.Drawing.Size(589, 352);
             ((System.ComponentModel.ISupportInitialize)(this.product_png)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox Company_txt;
-        private System.Windows.Forms.TextBox Outputnum_txt;
         private System.Windows.Forms.ComboBox Product_combo;
         private System.Windows.Forms.Button Start_btn;
         private System.Windows.Forms.Button Cancel_btn;
         private System.Windows.Forms.PictureBox product_png;
         public System.Windows.Forms.TextBox Serialnum_txt;
+        public System.Windows.Forms.TextBox Kcal_txt;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
     }
 }
