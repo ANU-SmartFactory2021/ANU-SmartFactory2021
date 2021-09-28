87997cac182f26e0d04602ebae2ba061c47fc926
jungyuuu <83994393+jungyuuu@users.noreply.github.com>
2021-09-28 13:20:03 +0900

    비전 qc 추가
    진행중

--- src/vision/qr.py
+++ src/vision/qr.py
@@ -6,8 +6,8 @@ from pyzbar import pyzbar
 
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, 320)
+cap.set(4, 320)
 
 
 def check():
@@ -23,6 +23,26 @@ def check():
         print(e)
 
 
+def qc(data):
+    gray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.GaussianBlur(gray,(5,5),0);
+	gray = cv2.medianBlur(gray,5)
+
+	gray = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+        cv2.THRESH_BINARY,11,3.5)
+    kernel = np.ones((2.6,2.7),np.uint8)
+	gray = cv2.erode(gray,kernel,iterations = 1)
+    gray = cv2.dilate(gray,kernel,iterations = 1)
+
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 260, param1=30, param2=65, minRadius=0, maxRadius=0)
+
+    if circles is not None:
+        return False
+    else:
+        return True
+
+
 def decode():
     retval, frame = cap.read()
     decoded_objects = pyzbar.decode(frame)
@@ -35,7 +55,12 @@ def decode():
     else:
         for obj in decoded_objects:
             print(obj.data)
-            return f'<QRCODE:{obj.data}|QUALITY=PASS|{converted_string.decode("utf-8")}>'
+            
+            if qc(buffer):
+                return f'<QRCODE:{obj.data}|QUALITY=PASS|{converted_string.decode("utf-8")}>'
+            else:
+                return f'<QRCODE:{obj.data}|QUALITY=FAIL|{converted_string.decode("utf-8")}>'
+
 
 # raspistill -v -o test.jpg
 # vcgencmd get_camera
